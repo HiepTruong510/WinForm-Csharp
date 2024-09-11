9420d0534aba47a24bd1d9fd6f84c7846fc122bf
UIT-20521313 <95198863+TruongVanHiep20521313@users.noreply.github.com>
2024-09-11 20:13:05 +0700

    third commit

---
+++ Image/combopicturebox/combopicturre/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combopicturre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadtenanh();
+            
+        }
+        void loadtenanh()
+        {
+            comboBox1.DataSource = new List<string>() { "1.jpg", "2.png", "3.png", "4.png" };
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            if (cb.SelectedValue != null)
+            {
+                pictureBox1.Image = new Bitmap(Application.StartupPath + "\\Resources\\" + cb.SelectedValue.ToString());
+            }    
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
