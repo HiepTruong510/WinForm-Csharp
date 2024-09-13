b7c9ced80fd543fc7ec07ecf626d2ae7aefc858c
UIT-20521313 <hieptv.cv@gmail.com>
2024-09-13 11:00:05 +0700

    add Graphic

---
+++ Graphic/Test1/Require.md
@@ -0,0 +1,5 @@
+Tải một hình ảnh lên, cho bốn lưới datagridview để hiển thị giá trị pixel của hình ảnh. 
+Thêm các button A,R,G,B
+- Khi ấn bất kì button nào nó sẽ hiển thị giá trị của từng pixel của button đó lên lưới datagridview tương ứng 
+(Ví dụ:  ấn button R nó sẽ chỉ hiển thị giá trị R của hình ảnh đó qua lưới datagridview tương ứng, Tương tự những button khác cũng vậy )
+- Khi bên lưới datagridview sau khi hiển thị ra giá trị, ta thay đổi giá trị trực tiếp trên lưới data đó và ấn load lại hình ảnh thì hình ảnh phải được thay đổi theo các giá trị đã sửa đổi 
---
+++ Graphic/Test1/truyenvaodatagridview/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace truyenvaodatagridview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnA = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnchonanh = new System.Windows.Forms.Button();
+            this.btnR = new System.Windows.Forms.Button();
+            this.btnG = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 311);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(291, 326);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(481, 120);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(87, 57);
+            this.btnA.TabIndex = 1;
+            this.btnA.Text = "giatri A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(369, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(297, 235);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnchonanh
+            // 
+            this.btnchonanh.Location = new System.Drawing.Point(37, 659);
+            this.btnchonanh.Name = "btnchonanh";
+            this.btnchonanh.Size = new System.Drawing.Size(291, 35);
+            this.btnchonanh.TabIndex = 3;
+            this.btnchonanh.Text = "chon anh";
+            this.btnchonanh.UseVisualStyleBackColor = true;
+            this.btnchonanh.Click += new System.EventHandler(this.btnchonanh_Click);
+            // 
+            // btnR
+            // 
+            this.btnR.Location = new System.Drawing.Point(816, 123);
+            this.btnR.Name = "btnR";
+            this.btnR.Size = new System.Drawing.Size(86, 50);
+            this.btnR.TabIndex = 4;
+            this.btnR.Text = "giatri R";
+            this.btnR.UseVisualStyleBackColor = true;
+            this.btnR.Click += new System.EventHandler(this.btnR_Click);
+            // 
+            // btnG
+            // 
+            this.btnG.Location = new System.Drawing.Point(468, 482);
+            this.btnG.Name = "btnG";
+            this.btnG.Size = new System.Drawing.Size(86, 50);
+            this.btnG.TabIndex = 5;
+            this.btnG.Text = "giatri G";
+            this.btnG.UseVisualStyleBackColor = true;
+            this.btnG.Click += new System.EventHandler(this.btnG_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(816, 482);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(86, 50);
+            this.btnB.TabIndex = 6;
+            this.btnB.Text = "giatri B";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(1064, 478);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(107, 54);
+            this.btn_load.TabIndex = 8;
+            this.btn_load.Text = "Load image";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.Location = new System.Drawing.Point(1186, 311);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(399, 383);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(715, 193);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(297, 235);
+            this.dataGridView2.TabIndex = 15;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(369, 553);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(297, 235);
+            this.dataGridView3.TabIndex = 16;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(715, 553);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.RowTemplate.Height = 24;
+            this.dataGridView4.Size = new System.Drawing.Size(297, 235);
+            this.dataGridView4.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1640, 625);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnG);
+            this.Controls.Add(this.btnR);
+            this.Controls.Add(this.btnchonanh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnchonanh;
+        private System.Windows.Forms.Button btnR;
+        private System.Windows.Forms.Button btnG;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+    }
+}
+
