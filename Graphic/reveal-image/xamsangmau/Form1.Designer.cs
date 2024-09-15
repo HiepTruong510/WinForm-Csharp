2a437190d4a0d9ac296b0d7802fcaadc07b4c049
UIT-20521313 <hieptv.cv@gmail.com>
2024-09-15 17:39:58 +0700

    add reveal-image in Graphic

---
+++ Graphic/reveal-image/xamsangmau/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace xamsangmau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(339, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 368);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "chon anh ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(339, 67);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(508, 493);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 642);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Graphic/reveal-image/xamsangmau/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xamsangmau
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string duongdan;
+            duongdan = openFileDialog1.FileName;
+
+            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Visible = false;
+            //-------------------------------------------//
+            Bitmap bitmap = (Bitmap)pictureBox1.Image;
+            Bitmap TrangDen = new Bitmap(bitmap.Width, bitmap.Height);
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int i = 0; i < bitmap.Height; i++)
+                {
+                    Color pixel = bitmap.GetPixel(x, i);
+                    byte b = pixel.B;
+                    byte g = pixel.G;
+                    byte r = pixel.R;
+                    byte xam = (byte)((r + g + b) / 3);
+                    TrangDen.SetPixel(x, i, Color.FromArgb(xam, xam, xam));
+                }
+           
+            }
+            pictureBox2.Image = TrangDen;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public void ha(int j)
+        {
+            Bitmap bitmap = (Bitmap)pictureBox1.Image;
+            Bitmap mau = new Bitmap(bitmap.Width, bitmap.Height);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int i = 0; i < bitmap.Height; i++)
+                {
+                    Color pixel = bitmap.GetPixel(x, i);
+                    byte b = pixel.B;
+                    byte g = pixel.G;
+                    byte r = pixel.R;
+                    byte xam = (byte)((r + g + b) / 3);
+                    int b2 = (int)(b - xam) / 10;
+                    int r2 = (int)(r - xam) / 10;
+                    int g2 = (int)(g - xam) / 10;
+
+                    mau.SetPixel(x, i, Color.FromArgb(xam + r2 * j, xam + g2 * j, xam + b2 * j));
+
+                    pictureBox2.Image = mau;
+
+                }
+            }
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();         
+        }
+        int j = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (j > 8)
+                timer1.Stop();
+            j = j + 1;
+            ha(j);
+            
+           
+        }
+    }
+}
